dccdcbb62fc357143e024c9c40f96396301d7288
Safwen Hafsawy <hafawysafwen@gmail.com>
2022-10-31 07:31:48 +0100

    finished home page ui

--- src/app/components/homePage/home.jsx
+++ src/app/components/homePage/home.jsx
@@ -7,13 +7,13 @@ export default function HomePage() {
     <div id="home-page">
       <div id="intro">
         <div id="home-headers-logo">
-          <h1>Welcome to Music </h1>
-          <h2>Home to your favorite music</h2>
+          <h1>Welcome to </h1>
+          <h3>Home to your favorite music</h3>
           <HomeBG />
         </div>
         <div id="get-started-action">
           <button>
-            <a href="#get-started-options">Get Started</a>
+            <a href="#get-started-options"> Get Started</a>
           </button>
         </div>
         <div className="separator">
@@ -21,13 +21,19 @@ export default function HomePage() {
         </div>
       </div>
       <div id="get-started-options">
+        <div id="get-started-intro">
+          <h4>
+            We offer you the opportunity to listen and enjoy your favorite music either by listening to
+            a set of music we selected for you or by connecting to your spotify
+            and bringing you favorite songs to Music <span>Bay</span>
+          </h4>
+          <h1>So what are you choosing today ? </h1>
+        </div>
         <div id="home-btns">
           <button>Start listening now</button>
           <button>Connect To Spotify</button>
         </div>
-        <div className="separator">
-          <HomeSeparator />
-        </div>
+
       </div>
     </div>
   );
